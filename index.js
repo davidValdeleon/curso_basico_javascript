74d99e8ce44d70cd030c03699ca9c635ed1af996
david_valdeleon <dvaldeleon123@gmail.com>
2023-01-06 12:17:25 -0500

    Se agregan los primeros archivos del curso basico de javascript a ver si por fin aprendo

---
+++ index.js
@@ -0,0 +1,23 @@
+// Variables
+var nombre = "David";
+
+// Declarar
+var edad;
+
+//inicializar = Para eso se necesita poner un igual
+edad = 30;
+
+edad
+
+// Estas son variables con cosas adentro
+
+var elementos = ['computadora', 'celular'];
+
+// Estas son variables con variables adentro que tienen diferentes asignaciones entre si
+
+var persona = {
+    nombre: 'David',
+    edad: 30
+}
+
+
