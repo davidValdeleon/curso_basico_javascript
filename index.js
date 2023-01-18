f890ab4fec4a6e725aee68050dee01026429fe0d
david_valdeleon <dvaldeleon123@gmail.com>
2023-01-18 11:07:23 -0500

    Se modifica index

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ var resultado = numero === 1 ? "Si soy un uno" : "No soy un uno";
 
 // vamos a hacer un juego de piedra papel o tijera
 
-// Primero se crea una constante con las opciones que se pueden escoger, luego se obtiene lo que el usuario escoja a partir de un prompt y por último se crea una opcion aleatoria por medio de Math.floor y el Math.random. Por ultimo se pone un if con los condicionales necesarios para poder validar
+// Primero se crea una constante con las opciones que se pueden escoger, luego se obtiene lo que el usuario escoja a partir de un prompt y por último se crea una opcion aleatoria por medio de Math.floor y el Math.random. Por ultimo se pone un if con los condicionales necesarios para poder validar quien gana o quien pierde
 
 const options = ['piedra','papel', 'tijera']
 const user = prompt('Elige piedra, papel o tijera');
@@ -110,4 +110,83 @@ user === 'tijera' && pc === 'papel'){
     console.log("ganaste");
 } else {
     console.log("perdiste")
+}
+
+//SWITCH (5) 
+
+var numero = 1;
+
+switch(numero) {
+    case 1:
+        console.log("Soy uno!")
+        break;
+    case 10:
+        console.log("Soy un diez!")
+        break;
+    case 100:
+        console.log("Soy un cien!")
+        break;
+    default:
+        console.log("No soy nada");
+}
+
+//Ahora el juego de piedra papel o tijera pero con el switch
+
+//INTENTO 1
+
+const options2 = ['piedra','papel', 'tijera']
+const user2 = prompt('Elige piedra, papel o tijera');
+const pc2 = 'piedra'
+// const pc2 = options2[Math.floor(Math.random() * options2.length)];
+
+
+switch(user2, pc2) {
+    case 'piedra','piedra':
+        console.log("empate")
+        break;
+    case 'papel','papel':
+        console.log("empate")
+        break;
+    case 'tijera','tijera':
+        console.log("empate")
+        break;
+
+    case 'piedra','tijera':
+        console.log("ganaste")
+        break;
+    case 'papel','piedra':
+        console.log("ganaste")
+        break;
+    case 'tijera','papel':
+        console.log("ganaste")
+        break;
+    default:
+        console.log("perdiste");
+}
+
+//INTENTO 2
+
+const options3 = ['piedra','papel', 'tijera'];
+const user3 = prompt('Elige piedra, papel o tijera');
+const pc3 = 'piedra';
+// options3[Math.floor(Math.random() * options3.length)];;
+
+
+switch(user3 === pc3) {
+    case 'piedra' === 'piedra'||'papel' === 'papel'||'tijera' === 'tijera':
+        console.log("empate");
+        break;
+    case 'piedra' === 'tijera':
+    case 'papel' === 'piedra':
+    case 'tijera' === 'papel':
+        console.log("ganaste");
+        break;
+    case 'piedra' === 'papel':
+    case 'papel' === 'tijera':
+    case 'tijera' === 'piedra':
+        console.log("perdiste");
+        break;
+    
+    default:
+        console.log("opcion invalida");
 }
